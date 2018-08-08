059d7099e4877fd562c85ce77f8d34b7b7bfca2d
DevExpressExampleBot <fakeEmail>
2018-08-08 14:51:20 +0300

    Source auto update [skip ci]

---
+++ VB/WpfSpreadsheet_CustomCommand/CustomCommandService.vb
@@ -0,0 +1,40 @@
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Spreadsheet
+Imports DevExpress.XtraSpreadsheet
+Imports DevExpress.XtraSpreadsheet.Commands
+Imports DevExpress.XtraSpreadsheet.Services
+Imports System
+Imports System.Windows
+
+Namespace WpfSpreadsheet_CustomCommand
+    Public Class CustomService
+        Inherits SpreadsheetCommandFactoryServiceWrapper
+
+        Public Sub New(ByVal service As ISpreadsheetCommandFactoryService)
+            MyBase.New(service)
+        End Sub
+        Public Property Control() As SpreadsheetControl
+
+        Public Overrides Function CreateCommand(ByVal id As SpreadsheetCommandId) As SpreadsheetCommand
+            If id Is SpreadsheetCommandId.FormatClearContents OrElse id Is SpreadsheetCommandId.FormatClearContentsContextMenuItem Then
+                Return New CustomFormatClearContentsCommand(Control)
+            End If
+            Return MyBase.CreateCommand(id)
+        End Function
+    End Class
+
+    Public Class CustomFormatClearContentsCommand
+        Inherits DevExpress.XtraSpreadsheet.Commands.FormatClearContentsCommand
+
+        Public Sub New(ByVal control As ISpreadsheetControl)
+            MyBase.New(control)
+        End Sub
+
+        Protected Overrides Sub ExecuteCore()
+            Dim result As MessageBoxResult = DXMessageBox.Show("Do you want to clear the cell content?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning)
+            If result = MessageBoxResult.Yes Then
+                MyBase.ExecuteCore()
+            End If
+        End Sub
+    End Class
+End Namespace
---
+++ VB/WpfSpreadsheet_CustomCommand/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.XtraSpreadsheet.Services
+Imports System
+
+Namespace WpfSpreadsheet_CustomCommand
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits ThemedWindow
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub spreadsheetControl_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dispatcher.BeginInvoke(New Action(Sub()
+                SubstituteService()
+            End Sub), System.Windows.Threading.DispatcherPriority.Render)
+        End Sub
+
+        Private Sub SubstituteService()
+            Dim service As ISpreadsheetCommandFactoryService = DirectCast(spreadsheetControl.GetService(GetType(ISpreadsheetCommandFactoryService)), ISpreadsheetCommandFactoryService)
+            Dim customService As New CustomService(service)
+            spreadsheetControl.ReplaceService(Of ISpreadsheetCommandFactoryService)(customService)
+            customService.Control = spreadsheetControl
+        End Sub
+    End Class
+End Namespace
