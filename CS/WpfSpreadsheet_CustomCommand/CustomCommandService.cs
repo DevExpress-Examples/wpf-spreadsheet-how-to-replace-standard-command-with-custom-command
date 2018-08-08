03435c7f03556d385c1c5b3d50b0a302d3e919ab
Natalya Senichkina <29920736+NatalyaSenichkina@users.noreply.github.com>
2018-08-08 14:31:20 +0300

    add example content

---
+++ CS/WpfSpreadsheet_CustomCommand/CustomCommandService.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Spreadsheet;
+using DevExpress.XtraSpreadsheet;
+using DevExpress.XtraSpreadsheet.Commands;
+using DevExpress.XtraSpreadsheet.Services;
+using System;
+using System.Windows;
+
+namespace WpfSpreadsheet_CustomCommand
+{
+    public class CustomService : SpreadsheetCommandFactoryServiceWrapper
+    {
+        public CustomService(ISpreadsheetCommandFactoryService service)
+            : base(service)
+        {
+        }
+        public SpreadsheetControl Control { get; set; }
+
+        public override SpreadsheetCommand CreateCommand(SpreadsheetCommandId id)
+        {
+            if (id == SpreadsheetCommandId.FormatClearContents || id == SpreadsheetCommandId.FormatClearContentsContextMenuItem)
+                return new CustomFormatClearContentsCommand(Control);
+            return base.CreateCommand(id);
+        }
+    }
+
+    public class CustomFormatClearContentsCommand : DevExpress.XtraSpreadsheet.Commands.FormatClearContentsCommand
+    {
+        public CustomFormatClearContentsCommand(ISpreadsheetControl control) :
+            base(control)
+        {
+        }
+
+        protected override void ExecuteCore()
+        {
+            MessageBoxResult result = DXMessageBox.Show("Do you want to clear the cell content?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+                base.ExecuteCore();
+        }
+    }
+}
---
+++ CS/WpfSpreadsheet_CustomCommand/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.XtraSpreadsheet.Services;
+using System;
+
+namespace WpfSpreadsheet_CustomCommand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void spreadsheetControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => {
+                SubstituteService();
+            }), System.Windows.Threading.DispatcherPriority.Render);
+        }
+
+        private void SubstituteService()
+        {
+            ISpreadsheetCommandFactoryService service = (ISpreadsheetCommandFactoryService)spreadsheetControl.GetService(typeof(ISpreadsheetCommandFactoryService));
+            CustomService customService = new CustomService(service);
+            spreadsheetControl.ReplaceService<ISpreadsheetCommandFactoryService>(customService);
+            customService.Control = spreadsheetControl;
+        }
+    }
+}
